3963fcdc66641c0211119c743ebf0a4b7a25bf8e
Oscar Mireles <oscar.mireles@grupobituaj.com.mx>
2025-10-03 17:18:19 -0600

    Agregando footer en front

--- src/app/vetLink-front/layouts/vetLink-front-layout/vetLink-front-layout.component.ts
+++ src/app/vetLink-front/layouts/vetLink-front-layout/vetLink-front-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import { FrontFooterComponent } from "../../components/front-footer/front-footer.component";
 
 @Component({
   selector: 'app-vet-link-front-layout',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FrontFooterComponent],
   templateUrl: './vetLink-front-layout.component.html',
 })
 export class VetLinkFrontLayoutComponent { }
